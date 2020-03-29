16e225b486bedb7c0e513a72da4e56cd0e5b5296
HOBIRON <yudainepnep@icloud.com>
2020-03-30 00:17:37 +0900

    Implement Profile Card frame

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import AppContext from '../../contexts/AppContext.js';
 import {
   CHANGE_PAGE_STATE_HOME,
 } from "../../actions/index";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Home.css";
 
 const Home = () => {
@@ -23,7 +24,46 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h1>Here is hohohohohohohoh</h1>
+      <div className="top" style={{ backgroundImage: "TopImage" }}>
+        <h1>Welcome to KamikamiLab!</h1>
+      </div>
+
+      <div className="profile-card">
+        <div className="profile-card-title">
+          <h3>About</h3>
+        </div>
+
+        <div className="profile-card-content">
+          <div className="profile-card-left">
+            <div className="profile-card-img">
+              img
+            </div>
+            <div className="profile-card-logos">
+              <a href="https://google.com" className="git">
+                <FontAwesomeIcon icon={["fab", "github-square"]} size={"3x"} className="logos" />
+              </a>
+              <a href="https://google.com" className="facebook">
+                <FontAwesomeIcon icon={["fab", "facebook-square"]} size={"3x"} className="logos" />
+              </a>
+              <a href="https://google.com" className="twitter">
+                <FontAwesomeIcon icon={["fab", "twitter-square"]} size={"3x"} className="logos" />
+              </a>
+              <a href="https://google.com" className="youtube">
+                <FontAwesomeIcon icon={["fab", "youtube"]} size={"3x"} className="logos" />
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-card-right">
+            <h3 className="profile-card-right-name">川上 雄大（Yudai Kawakami）</h3>
+            <p className="profile-card-right-content">
+              こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは
+            </p>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 };
