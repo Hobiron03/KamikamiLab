a0e778e8910b323137913f2274eeb39188889175
HOBIRON <yudainepnep@icloud.com>
2020-04-06 16:57:34 +0900

    take care of smartphone screen

--- src/components/Works/Works.tsx
+++ src/components/Works/Works.tsx
@@ -1,4 +1,11 @@
 import React, { useEffect, useContext, useState, useCallback } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from "react-router-dom";
 import AppContext from '../../contexts/AppContext.js';
 import {
   CHANGE_PAGE_STATE_WORKS,
@@ -7,10 +14,16 @@ import "./Works.css";
 
 const Works = () => {
 
+  let match = useRouteMatch();
+
+  console.log(match);
+
   const { dispatch } = useContext(AppContext);
 
   const [pageState, setPageState] = useState([true, false, false]);
   const stableDispatch = useCallback(dispatch, [pageState]);
+
+  const [toggleState, setToggleState] = useState("product");
 
   useEffect(() => {
     setPageState([true, false, false]);
@@ -28,10 +41,28 @@ const Works = () => {
   //   });
   // }
 
+  const changeToggleState = (toggle: string): void => {
+    setToggleState(toggle);
+  }
+
   return (
     <div className="Works">
-      <h1>Here is Works</h1>
-    </div>
+      <h1 className="title">Works</h1>
+      <div className="toggle-button">
+        <div className="toggle-button-product toggle-button-content"
+          onClick={e => changeToggleState("product")}>
+          <p>Product</p>
+        </div>
+        <div className="toggle-button-music toggle-button-content"
+          onClick={e => changeToggleState("music")}>
+          <p>Music</p>
+        </div>
+        <div className="toggle-button-youtube toggle-button-content"
+          onClick={e => changeToggleState("youtube")}>
+          <p>Youtube</p>
+        </div>
+      </div>
+    </div >
   )
 };
 
